1cb91c021be2c39c782777e900c12885e269de41
GabrielCF2 <gabrielcotrimbatata@gmail.com>
2024-03-09 16:13:52 -0300

    Signing in

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -19,10 +19,9 @@ from django.conf.urls.static import static
 #Não use o que está a cima em produção
 from django.contrib import admin
 from django.urls import path,include
-from core.views import index,contact
+
 urlpatterns = [
-    path("",index,name="index"),
+    path("",include('core.urls')),
     path("items/",include('item.urls')),
-    path("contact/",contact,name="contact"),
     path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
